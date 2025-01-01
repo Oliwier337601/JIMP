7e3a2499df060623773fe98556e3841c4f78e461
pwimperator <01197791@pw.edu.pl>
2025-01-01 19:53:39 +0100

    implementation of back substitution

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,18 +5,37 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+	if (mat->r != mat->c) return 2;
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+	Matrix * temp = createMatrix(mat->r, 1);
+	int row;
+	for (row = mat->r - 1; row >= 0; row--)
+	{
+		if (mat->data[row][row] == 0)
+		{
+			freeMatrix(temp);
+			return 1;
+		}
 
-				return 0;
+		double res = b->data[row][0];
+		int col;
+		for (col = row + 1; col < mat->c; col++)
+		{
+			res -= temp->data[col][0] * mat->data[row][col];
+		}
+		res /= mat->data[row][row];
+		temp->data[row][0] = res;
+	
+	}
+
+	int i;
+	for (i =0; i < x->r; i++) 
+	{
+		x->data[i][0] = temp->data[i][0];
+	}
+	freeMatrix(temp);
+	return 0;
 }
 
 
