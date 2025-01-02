c9d1d7e1c48f836df51b05a08657a6880960d047
Oliwier337601 <01197841@pw.edu.pl>
2025-01-02 19:49:54 +0100

    main corrections

--- src/main.c
+++ src/main.c
@@ -5,27 +5,41 @@
 #include <stdio.h>
 
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
 	int res;
-	Matrix * A = readFromFile(argv[1]);
-	Matrix * b = readFromFile(argv[2]);
-	Matrix * x;
+	Matrix *A = readFromFile(argv[1]);
+	Matrix *b = readFromFile(argv[2]);
+	Matrix *x;
 
 	if (A == NULL) return -1;
 	if (b == NULL) return -2;
 	printToScreen(A);
 	printToScreen(b);
 
-	res = eliminate(A,b);
+	res = eliminate(A, b);
+    if (res == 1) {
+        fprintf(stderr, "Błąd! Macierz osobliwa.\n");
+        return 1;
+    }
+    
 	x = createMatrix(b->r, 1);
-	if (x != NULL) {
-		res = backsubst(x,A,b);
+    if (x == NULL) {
+        fprintf(stderr, "Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+        return 1;
+    }
+    
+    res = backsubst(x, A, b);
+    if (res == 1) {
+        fprintf(stderr, "Błąd! Dzielenie przez 0.\n");
+        return 1;
+    } else if (res == 2) {
+        fprintf(stderr,"Błąd! Macierz nie jest kwadratowa.\n");
+        return 1;
+    }
 
-		printToScreen(x);
-	  freeMatrix(x);
-	} else {
-					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
-	}
+    printToScreen(x);
+    freeMatrix(x);
+	
 
 	freeMatrix(A);
 	freeMatrix(b);
